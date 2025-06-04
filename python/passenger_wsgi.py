294d210f1d0ada20b5721f5cc7915ac409457192
aktilokchandani <aktilokchandani@hotmail.com>
2025-06-04 19:20:25 -0400

    activate listener flow

--- python/passenger_wsgi.py
+++ python/passenger_wsgi.py
@@ -1,13 +1,9 @@
+import imp
 import os
 import sys
 
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+wsgi = imp.load_source('wsgi', 'test_audio_features.py')
+application = wsgi.application
